f1e225f95e1edcfcd15de8080f2fc54a6d14c77f
jardeson777 <jardeson777@gmail.com>
2022-11-25 00:48:34 -0300

    estruturando projeto DDD

---
+++ cursos/Cursos.Domain/Aluno.cs
@@ -0,0 +1,9 @@
+﻿namespace Cursos.Domain
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ cursos/Cursos.Domain/Turma.cs
@@ -0,0 +1,8 @@
+﻿namespace Cursos.Domain
+{
+    public class Turma
+    {
+        public string Numero { get; set; }
+        public string AnoLetivo { get; set; }
+    }
+}
