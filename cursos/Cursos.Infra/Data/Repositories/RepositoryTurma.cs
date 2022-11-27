12b5a486ed784fced9f6701b8072ebc4baa63ed1
jardeson777 <jardeson777@gmail.com>
2022-11-27 16:15:40 -0300

    criando infra para repositorio de aluno e turma

---
+++ cursos/Cursos.Infra/Data/Repositories/RepositoryTurma.cs
@@ -0,0 +1,15 @@
+﻿using Cursos.Domain;
+using Cursos.Domain.Core.Interfaces.Repositories;
+
+namespace Cursos.Infra.Data.Repositories
+{
+    public class RepositoryTurma : RepositoryBase<Turma>, IRepositoryTurma
+    {
+        private readonly SqlContext sqlContext;
+
+        public RepositoryTurma(SqlContext sqlContext) : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+    }
+}
